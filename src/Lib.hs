bb3367f7ded22e8ab58e2789e36d114a8fcd8591
Clearly Not A Girl <clearlynotagirl@twitch.tv>
2020-12-29 14:36:22 -0800

    Add dummy implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,19 @@
 module Lib
-    ( someFunc
-    ) where
+where
 
 import ClassyPrelude
+import Domain.Auth
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- Throwaway code
+
+instance AuthRepo IO where
+  addAuth (Auth email pass) = do
+    putStrLn $ "Adding auth: " <> rawEmail email
+    return $ Right "fake verification code"
+
+instance EmailVerificationNotifier IO where
+  notifyEmailVerification email vcode =
+    putStrLn $ "Notify " <> rawEmail email <> " - " <> vcode
