3ef5f617b510036aedebc74b2eace5555d2d965a
Clearly Not A Girl <clearlynotagirl@twitch.tv>
2020-12-29 14:05:10 -0800

    Add domain functions

---
+++ src/Domain/Auth.hs
@@ -0,0 +1,76 @@
+module Domain.Auth where
+
+import ClassyPrelude
+import Control.Monad.Except
+import Domain.Validation
+import Text.Regex.PCRE.Heavy
+
+data RegistrationError
+  = RegistrationErrorEmailTaken
+  deriving (Show, Eq)
+
+-- Email-related types
+
+newtype Email = Email {emailRaw :: Text} deriving (Show, Eq)
+
+rawEmail :: Email -> Text
+rawEmail = emailRaw
+
+mkEmail :: Text -> Either [EmailValidationErr] Email
+mkEmail =
+  validate
+    Email
+    [ regexMatches
+        [re|^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$|]
+        EmailValidationErrInvalidEmail
+    ]
+
+data EmailValidationErr = EmailValidationErrInvalidEmail deriving (Show, Eq)
+
+-- Password-related types
+
+newtype Password = Password {passwordRaw :: Text} deriving (Show, Eq)
+
+rawPassword :: Password -> Text
+rawPassword = passwordRaw
+
+mkPassword :: Text -> Either [PasswordValidationErr] Password
+mkPassword =
+  validate
+    Password
+    [ lengthBetween 5 50 PasswordValidationErrLength,
+      regexMatches [re|\d|] PasswordValidationErrMustContainNumber,
+      regexMatches [re|[A-Z]|] PasswordValidationErrMustContainUpperCase,
+      regexMatches [re|[a-z]|] PasswordValidationErrMustContainLowerCase
+    ]
+
+data PasswordValidationErr
+  = PasswordValidationErrLength
+  | PasswordValidationErrMustContainUpperCase
+  | PasswordValidationErrMustContainLowerCase
+  | PasswordValidationErrMustContainNumber
+  deriving (Show, Eq)
+
+-- Auth-related types
+
+data Auth = Auth
+  { authEmail :: Email,
+    authPassword :: Password
+  }
+  deriving (Show, Eq)
+
+-- Registration
+
+type VerificationCode = Text
+
+class Monad m => AuthRepo m where
+  addAuth :: Auth -> m (Either RegistrationError VerificationCode)
+
+class Monad m => EmailVerificationNotifier m where
+  notifyEmailVerification :: Email -> VerificationCode -> m ()
+
+register :: (AuthRepo m, EmailVerificationNotifier m) => Auth -> m (Either RegistrationError ())
+register auth = runExceptT $ do
+  vCode <- ExceptT $ addAuth auth
+  let email = authEmail auth
+  lift $ notifyEmailVerification email vCode
---
+++ src/Domain/Validation.hs
@@ -0,0 +1,25 @@
+module Domain.Validation where
+
+import ClassyPrelude
+import Text.Regex.PCRE.Heavy
+
+type Validation e a = a -> Maybe e
+
+validate :: (a -> b) -> [Validation e a] -> a -> Either [e] b
+validate builder validations value =
+  case concatMap (\f -> maybeToList $ f value) validations of
+    [] -> Right $ builder value
+    errs -> Left errs
+
+rangeBetween :: (Ord a) => a -> a -> e -> Validation e a
+rangeBetween min max msg val =
+  if val >= min && val <= max then Nothing else Just msg
+
+lengthBetween :: (MonoFoldable a) => Int -> Int -> e -> Validation e a
+lengthBetween minLen maxLen msg val =
+  rangeBetween minLen maxLen msg (length val)
+
+regexMatches :: Regex -> e -> Validation e Text
+regexMatches regex msg val =
+  if val =~ regex then Nothing else Just msg
+
